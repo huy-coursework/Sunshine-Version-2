2aad055042f837da93f7238146c9b889fd3395a1
Huy Vuong <huy-vuong@users.noreply.github.com>
2016-12-14 01:49:31 -0800

    5.15 Implement insert() in WeatherProvider

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -238,10 +238,22 @@ public class WeatherProvider extends ContentProvider {
             case WEATHER: {
                 normalizeDate(values);
                 long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, values);
-                if ( _id > 0 )
+                if (_id > 0)
                     returnUri = WeatherContract.WeatherEntry.buildWeatherUri(_id);
                 else
                     throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            case LOCATION: {
+                long _id = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                {
+                    returnUri = WeatherContract.LocationEntry.buildLocationUri(_id);
+                }
+                else
+                {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
                 break;
             }
             default:
